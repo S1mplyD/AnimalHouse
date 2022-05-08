557ea53d8c6900f039ae2cd3f2725daa72592ef8
S1mplyD <lucabennati2k16@gmail.com>
2022-05-08 22:38:32 +0200

    move endpoint to route

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "../.env" });
-const Product = require("./models/product.model");
+const productsRoute = require("./routes/products");
 
 const app = express();
 
@@ -11,34 +11,10 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 
+//Products routes
+app.use("/products", productsRoute);
+
 mongoose.connect(process.env.MONGODB_PERSONAL_URI);
-//Check mongo connection
-// mongoose.connection.once("open", () => {
-//   console.log("connessione avvenuta con successo");
-// });
-
-app.post("/api/addproduct", async (req, res) => {
-  console.log(req.body);
-
-  try {
-    const product = await Product.create({
-      name: req.body.name,
-      description: req.body.description,
-      price: req.body.price,
-      id: req.body.id,
-    });
-    res.sendStatus(200);
-  } catch (error) {
-    console.log("Error: " + error);
-    res.sendStatus(400);
-  }
-});
-
-app.get("/api/products", async (req, res) => {
-  const products = await Product.find();
-  console.log(products);
-  res.send(products);
-});
 
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
