bf76b9dd046d0859ee076f3bd72d954c9ddcfd76
S1mplyD <lucabennati2k16@gmail.com>
2022-05-09 13:43:03 +0200

    remove console logs and add error response

--- server/routes/products.js
+++ server/routes/products.js
@@ -9,7 +9,6 @@ router
    */
   .get(async (req, res) => {
     const products = await Product.find();
-    console.log(products);
     res.send(products);
   })
   /**
@@ -17,8 +16,6 @@ router
    * Create a new product
    */
   .post(async (req, res) => {
-    console.log(req.body);
-
     try {
       await Product.create({
         name: req.body.name,
@@ -28,8 +25,7 @@ router
       });
       res.sendStatus(200);
     } catch (error) {
-      console.log("Error: " + error);
-      res.sendStatus(400);
+      res.json({ Error: "Error" + error });
     }
   });
 
@@ -40,12 +36,11 @@ router
  * @param productid Id of the product (uses _id field of mongodb)
  */
 router.route("/:productid").put(async (req, res) => {
-  console.log(req.params.productid);
   try {
     await Product.findByIdAndUpdate(req.params.productid, req.body);
     res.status(200).json("Product edited successfully");
   } catch (error) {
-    //throw error;
+    res.json({ Error: "Error" + error });
   }
 });
 
@@ -62,7 +57,7 @@ router
       const product = await Product.findOne({ name: req.params.productname });
       res.status(200).json(product);
     } catch (error) {
-      throw error;
+      res.json({ Error: "Error" + error });
     }
   });
 
