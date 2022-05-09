e4b034a902153cd77064c8ae70462ce43204e09f
S1mplyD <lucabennati2k16@gmail.com>
2022-05-09 12:32:26 +0200

    add search by name and edit by id

--- server/routes/products.js
+++ server/routes/products.js
@@ -3,16 +3,24 @@ const Product = require("../models/product.model");
 
 router
   .route("/")
+  /**
+   * GET
+   * Get all products
+   */
   .get(async (req, res) => {
     const products = await Product.find();
     console.log(products);
     res.send(products);
   })
+  /**
+   * POST
+   * Create a new product
+   */
   .post(async (req, res) => {
     console.log(req.body);
 
     try {
-      const product = await Product.create({
+      await Product.create({
         name: req.body.name,
         description: req.body.description,
         price: req.body.price,
@@ -24,17 +32,38 @@ router
       res.sendStatus(400);
     }
   });
+
+/**
+ * PUT
+ * Change a property of a product
+ *
+ * @param productid Id of the product (uses _id field of mongodb)
+ */
 router.route("/:productid").put(async (req, res) => {
-  console.log(req.params.productid + " " + req.body.name);
-  let str = "6277941a071a312833557bdd";
+  console.log(req.params.productid);
   try {
-    await Product.findByIdAndUpdate(req.params.productid, {
-      name: req.body.name,
-    });
-    res.sendStatus(200);
+    await Product.findByIdAndUpdate(req.params.productid, req.body);
+    res.status(200).json("Product edited successfully");
   } catch (error) {
-    console.log(error);
+    //throw error;
   }
 });
+
+router
+  .route("/:productname")
+  /**
+   * GET
+   * Get by product by name
+   *
+   * @param productname name of the product we search for
+   */
+  .get(async (req, res) => {
+    try {
+      const product = await Product.findOne({ name: req.params.productname });
+      res.status(200).json(product);
+    } catch (error) {
+      throw error;
+    }
+  });
 
 module.exports = router;
