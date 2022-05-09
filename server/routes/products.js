5b05020b1f2b848650d7c725662ae85323414067
S1mplyD <lucabennati2k16@gmail.com>
2022-05-09 15:37:18 +0200

    add delete function

--- server/routes/products.js
+++ server/routes/products.js
@@ -29,20 +29,36 @@ router
     }
   });
 
-/**
- * PUT
- * Change a property of a product
- *
- * @param productid Id of the product (uses _id field of mongodb)
- */
-router.route("/:productid").put(async (req, res) => {
-  try {
-    await Product.findByIdAndUpdate(req.params.productid, req.body);
-    res.status(200).json("Product edited successfully");
-  } catch (error) {
-    res.json({ Error: "Error" + error });
-  }
-});
+router
+  .route("/:productid")
+  /**
+   * PUT
+   * Change a property of a product
+   *
+   * @param productid Id of the product (uses _id field of mongodb)
+   */
+  .put(async (req, res) => {
+    try {
+      await Product.findByIdAndUpdate(req.params.productid, req.body);
+      res.status(200).json("Product edited successfully");
+    } catch (error) {
+      res.json({ Error: "Error" + error });
+    }
+  })
+  /**
+   * DELETE
+   * Delete an item with a specific id
+   *
+   * @param productid Id of the product (uses _id field of mongodb)
+   */
+  .delete(async (req, res) => {
+    try {
+      await Product.findByIdAndDelete(req.params.productid);
+      res.status(200).json("Product deleted successfully");
+    } catch (error) {
+      res.json({ Error: "Error" + error });
+    }
+  });
 
 router
   .route("/:productname")
