5a85fd8cffc9f4843ccf0e310988f8fc791b8c03
S1mplyD <lucabennati2k16@gmail.com>
2022-05-10 17:16:55 +0200

    translation

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -5,10 +5,10 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     surname: { type: String, required: true },
     username: { type: String, required: true, unique: true },
-    mail: { type: String, required: true, unique: true }, //login purpose only
+    mail: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    ownedAnimals: { type: [String] }, //_ids of owned animals
-    profilePicture: { type: String }, //Path to image on fs
+    ownedAnimals: { type: [String] }, //Id degli animali posseduti (_id di mongo)
+    profilePicture: { type: String }, //Path alle immagini su filesystem
   },
   {
     collection: "user-data",
