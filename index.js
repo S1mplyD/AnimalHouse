47744e5fcccec173759bb95b5e4a4e303aa0fe76
S1mplyD <lucabennati2k16@gmail.com>
2022-05-10 18:59:11 +0200

    add holidays fetching

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const productsRoute = require("./server/routes/products");
 const petsRoute = require("./server/routes/pets");
 const usersRoute = require("./server/routes/users");
 const leaderboardRoute = require("./server/routes/leaderboard");
+const getHolidays = require("./utilities/holidays").getHolidays;
 
 const app = express();
 
@@ -22,6 +23,14 @@ app.use("/api/products", productsRoute);
 app.use("/api/pets", petsRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/leaderboard", leaderboardRoute);
+app.get("/api/getHolidays", async (req, res) => {
+  const holiday = await getHolidays(
+    req.body.day,
+    req.body.month,
+    req.body.year
+  );
+  res.json(holiday);
+});
 
 mongoose.connect(process.env.MONGODB_PERSONAL_URI);
 
