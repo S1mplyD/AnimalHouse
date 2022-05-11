16e9598d3e5536455c908f7cd69e89b75013b840
S1mplyD <lucabennati2k16@gmail.com>
2022-05-11 18:59:48 +0200

    refactor

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    surname: { type: String, required: true },
+    name: { type: String, required: true }, //Nome completo (nome e cognome)
     username: { type: String, required: true, unique: true },
-    mail: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    mail: { type: String, unique: true },
+    serviceId: { type: String, unique: true }, //Id del servizio di login usato (E.s.: Google, Twitter...)
+    password: { type: String },
     ownedAnimals: { type: [String] }, //Id degli animali posseduti (_id di mongo)
     profilePicture: { type: String }, //Path alle immagini su filesystem
   },
