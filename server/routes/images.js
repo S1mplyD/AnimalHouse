bc869636ab0e74d7564e70f39b1a17a38b7a50cf
S1mplyD <lucabennati2k16@gmail.com>
2022-05-16 11:32:56 +0200

    fix routing, add unlink of old images, change method to pass images to frontend

--- server/routes/images.js
+++ server/routes/images.js
@@ -14,65 +14,94 @@ const storage = multer.diskStorage({
   },
 });
 
-router.post("/avatar", async (req, res) => {
-  if (req.user != null) {
-    const upload = multer({ storage: storage }).single("image");
-    upload(req, res, async function (err) {
-      if (err) {
-        console.log(err);
-      } else {
-        await User.findOneAndUpdate(
-          { username: req.user.username },
-          {
-            profilePicture: res.req.file.filename,
-          }
-        );
-      }
-    });
+router
+  .route("/avatar")
+  .get(async (req, res) => {
+    if (req.user != null) {
+      await User.findOne({ username: req.user.username }).then((user) => {
+        if (user) {
+          res.send(user.profilePicture);
+        } else {
+          res.json("not authorized");
+        }
+      });
+    } else {
+      res.json("not logged in");
+    }
+  })
+  .post(async (req, res) => {
+    if (req.user != null) {
+      const upload = multer({ storage: storage }).single("image");
+      upload(req, res, async function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          await User.findOne({ username: req.user.username }).then(
+            async (user) => {
+              await fs.unlink(
+                __foldername + "/server/Images/" + user.profilePicture,
+                (err) => {
+                  if (err) console.log(err);
+                }
+              );
+            }
+          );
+          await User.findOneAndUpdate(
+            { username: req.user.username },
+            {
+              profilePicture: res.req.file.filename,
+            }
+          );
+        }
+      });
 
-    res.redirect(200, "/");
-  } else {
-    res.json("non loggato");
-  }
-});
+      res.redirect(200, "/");
+    } else {
+      res.json("non loggato");
+    }
+  });
 
-//TODO aggiungi l'immagine solo all'animale specifico con controllo dell'utente
-router.post("/petPhotos/:petid", async (req, res) => {
-  if (req.user != null) {
-    const upload = multer({ storage: storage }).array("images", 10);
-    upload(req, res, async (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.req.files.forEach((element) => {
-          console.log(element.filename);
-        });
-        // await Pet.findOneAndUpdate(
-        //   { $and: [{ owner: req.user.username }, { _id: req.params.petid }] },
-        //   {
-        //     profilePicture: "http://localhost:8000/api/images/" + res.req.files,
-        //   }
-        // );
-      }
-    });
+router
+  .route("/petPhotos/:petid")
+  .get(async (req, res) => {
+    if (req.user != null) {
+      await Pet.findOne({ _id: req.params.petid }).then((pet) => {
+        if (pet) {
+          res.send(pet.pictures);
+        }
+      });
+    }
+  })
+  .post(async (req, res) => {
+    if (req.user != null) {
+      const upload = multer({ storage: storage }).array("images", 10);
+      upload(req, res, async (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.req.files.forEach((element) => {
+            console.log(element.filename);
+          });
+          const imagesAddr = [];
+          res.req.files.forEach((element) => {
+            imagesAddr.push(element.filename);
+          });
+          await Pet.findOneAndUpdate(
+            { $and: [{ owner: req.user.username }, { _id: req.params.petid }] },
+            {
+              $push: {
+                pictures: {
+                  $each: imagesAddr,
+                },
+              },
+            }
+          );
+          res.redirect(200, "/");
+        }
+      });
+    } else {
+      res.json("non loggato");
+    }
+  });
 
-    res.json("immagine profilo aggiornata");
-  } else {
-    res.json("non loggato");
-  }
-});
-
-router.get("/avatar", async (req, res) => {
-  if (req.user != null) {
-    await User.findOne({ username: req.user.username }).then((user) => {
-      if (user) {
-        res.sendFile(__foldername + "/server/Images/" + user.profilePicture);
-      } else {
-        res.json("not authorized");
-      }
-    });
-  } else {
-    res.json("not logged in");
-  }
-});
 module.exports = router;
