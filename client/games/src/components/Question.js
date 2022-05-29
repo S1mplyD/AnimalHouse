b3004a3f924e72c0254528b272886d966b033f61
S1mplyD <lucabennati2k16@gmail.com>
2022-05-29 19:07:46 +0200

    add question component

--- client/games/src/components/Question.js
+++ client/games/src/components/Question.js
@@ -1,5 +1,22 @@
-function Question(){
+import React, { useEffect } from "react";
+const htmlDecoder = require("html-encoder-decoder").decode;
 
+function Question({ questions, setQuestions, score, setScore }) {
+  useEffect(() => {
+    console.log(questions);
+  }, [questions]);
+  return (
+    //  question.question, question.correct_answer, question.incorrect_answers
+    //  TODO: ordine risposte casuale, trasformare simboli in caratteri
+    <div>
+      <p>test</p>
+      {/* <h2 id="question">{htmlDecoder(question.question)}</h2>
+      <p id="answers">{htmlDecoder(question.correct_answer)}</p>
+      <p id="answers">{htmlDecoder(question.incorrect_answers[0])}</p>
+      <p id="answers">{htmlDecoder(question.incorrect_answers[1])}</p>
+      <p id="answers">{htmlDecoder(question.incorrect_answers[2])}</p> */}
+    </div>
+  );
 }
 
-export default Question
+export default Question;
