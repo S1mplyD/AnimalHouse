4318b8104ed0b3895c805c3fb8c29396303ee5a5
S1mplyD <lucabennati2k16@gmail.com>
2022-05-29 19:05:42 +0200

    add shuffle

--- client/games/src/components/Quiz.js
+++ client/games/src/components/Quiz.js
@@ -1,33 +1,33 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
+import Question from "./Question";
+import decode from "html-encoder-decoder";
 
-function Quiz(){
+function Quiz({ questions, setQuestions, score, setScore }) {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [options, setOptions] = useState([]);
+  useEffect(() => {
+    setOptions(
+      questions &&
+        handleShuffle([
+          questions[currentQuestion].correct_answer,
+          ...questions[currentQuestion].incorrect_answers,
+        ])
+    );
+  }, [currentQuestion, questions]);
+  const handleShuffle = (options) => {
+    return options.sort(() => Math.random() - 0.5);
+  };
 
-    useEffect(()=>{
-        console.log("here")
-        getQuestion();
-    }, [])
-
-    const [questions, setQuestions] = useState([]);
-
-    const getQuestion = async () =>{
-        console.log("here2")
-        const rawData = await fetch("http://localhost:8000/api/getTrivia/medium")
-        const data = await rawData.json()
-        setQuestions(data)
-    } 
-
-    return(
-        <div>
-            
-            {console.log(questions)
-            /* {
-            questions.map(question =>{
-                console.log(question.question)
-            })} */}
-        </div>
-        
-
-    )
+  return (
+    <div>
+      {decode.decode(questions[currentQuestion].question)}
+      <div>
+        {options.map((i) => (
+          <p>{i}</p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Quiz
+export default Quiz;
