3f5e87c83b65389e2cb3e8e9eaef3d2436aaa1b8
S1mplyD <lucabennati2k16@gmail.com>
2022-06-05 18:26:47 +0200

    add question logic and styles

--- client/games/src/components/Question.js
+++ client/games/src/components/Question.js
@@ -1,22 +1,87 @@
-import React, { useEffect } from "react";
-const htmlDecoder = require("html-encoder-decoder").decode;
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import "../componentsCss/Question.css";
+import decode from "html-encoder-decoder";
 
-function Question({ questions, setQuestions, score, setScore }) {
-  useEffect(() => {
-    console.log(questions);
-  }, [questions]);
+const Question = ({
+  currentQuestion,
+  setCurrentQuestion,
+  questions,
+  options,
+  correct,
+  setScore,
+  score,
+  setQuestions,
+}) => {
+  const [selected, setSelected] = useState();
+  const [error, setError] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSelect = (i) => {
+    if (selected === i && selected === correct) return "select";
+    else if (selected === i && selected !== correct) return "wrong";
+    else if (i === correct) return "select";
+  };
+
+  const handleCheck = (i) => {
+    setSelected(i);
+    if (i === correct) setScore(score + 1);
+    setError(false);
+  };
+
+  const handleNext = () => {
+    if (currentQuestion > 3) {
+      navigate("/result");
+    } else if (selected) {
+      setCurrentQuestion(currentQuestion + 1);
+      setSelected();
+    } else setError("Please select an option first");
+  };
+
+  const handleQuit = () => {
+    setCurrentQuestion(0);
+    setQuestions();
+  };
+
   return (
-    //  question.question, question.correct_answer, question.incorrect_answers
-    //  TODO: ordine risposte casuale, trasformare simboli in caratteri
-    <div>
-      <p>test</p>
-      {/* <h2 id="question">{htmlDecoder(question.question)}</h2>
-      <p id="answers">{htmlDecoder(question.correct_answer)}</p>
-      <p id="answers">{htmlDecoder(question.incorrect_answers[0])}</p>
-      <p id="answers">{htmlDecoder(question.incorrect_answers[1])}</p>
-      <p id="answers">{htmlDecoder(question.incorrect_answers[2])}</p> */}
+    <div className="question">
+      <h1 className="questionNumber">Question {currentQuestion + 1} :</h1>
+      <div className="singleQuestion">
+        <h2 className="questionNumber">
+          {decode.decode(questions[currentQuestion].question)}
+        </h2>
+        <div className="options">
+          {options &&
+            options.map((i) => (
+              <button
+                className={`singleOption  ${selected && handleSelect(i)}`}
+                key={i}
+                onClick={() => handleCheck(i)}
+                disabled={selected}
+              >
+                {i}
+              </button>
+            ))}
+        </div>
+        <div className="controls">
+          <button
+            className="btnControl"
+            onClick={() => {
+              window.location.href = "/";
+              handleQuit();
+            }}
+          >
+            Quit
+          </button>
+
+          <button className="btnControl" onClick={handleNext}>
+            {currentQuestion > 3 ? "Submit" : "Next Question"}
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Question;
