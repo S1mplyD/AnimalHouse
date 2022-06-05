513ece764a0828607df0d4c43d4e07a066ec8696
S1mplyD <lucabennati2k16@gmail.com>
2022-06-05 18:26:26 +0200

    change history to navigate

--- client/games/src/components/Home.js
+++ client/games/src/components/Home.js
@@ -1,19 +1,18 @@
 import image from "../1652541244672.png";
 import { useNavigate, Link } from "react-router-dom";
 import React from "react";
+import "../componentsCss/Home.css";
 
 function Home({ getQuestions, setScore, setQuestions }) {
-  const history = useNavigate();
+  const navigate = useNavigate();
   const handleQuiz = async () => {
     await getQuestions();
-    history("/quiz");
+    navigate("/quiz");
   };
-  const score = () => {
-    setScore(10);
-  };
+
   return (
     <div className="games">
-      <img src={image} alt="quiz" onClick={handleQuiz} />
+      <img className="image" src={image} alt="quiz" onClick={handleQuiz} />
     </div>
   );
 }
