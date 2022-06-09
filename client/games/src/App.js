3ea4a9ea15c40fd335f27c218abec102cbe5fe3e
S1mplyD <lucabennati2k16@gmail.com>
2022-06-09 17:42:50 +0200

    add image fetch

--- client/games/src/App.js
+++ client/games/src/App.js
@@ -2,18 +2,33 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import React, { useState } from "react";
 import axios from "axios";
-import Quiz from "./components/Quiz";
+import Quiz from "./components/Quiz/Quiz";
 import Home from "./components/Home";
-import Result from "./components/Result";
+import Result from "./components/Quiz/Result";
+import Memory from "./components/Memory/Memory";
 
 function App() {
   const [questions, setQuestions] = useState([]);
   const [score, setScore] = useState(0);
+  const [images, setImages] = useState([]);
   const getQuestions = async () => {
     const rawData = await axios.get(
       "http://192.168.8.176:8000/api/getTrivia/medium"
     );
     setQuestions(rawData.data);
+  };
+
+  const getImages = async () => {
+    const data = await axios.get(
+      "https://zoo-animal-api.herokuapp.com/animals/rand/8"
+    );
+    let memoryImages = [];
+    data.data.map((i) => {
+      memoryImages.push({ image: i.image_link, state: "" });
+      memoryImages.push({ image: i.image_link, state: "" });
+    });
+    memoryImages.sort(() => Math.random() - 0.5);
+    setImages(memoryImages);
   };
 
   return (
@@ -30,6 +45,7 @@ function App() {
                 setQuestions={setQuestions}
                 getQuestions={getQuestions}
                 setScore={setScore}
+                getImages={getImages}
               />
             }
           />
@@ -45,6 +61,7 @@ function App() {
             }
           />
           <Route path="/result" element={<Result score={score} />} />
+          <Route path="/memory" element={<Memory images={images} />}></Route>
         </Routes>
       </div>
     </Router>
