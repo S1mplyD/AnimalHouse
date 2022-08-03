6940156302fe98f3a7463921bf7c6282b6189aa8
Luca Bennati <lucabennati2k16@gmail.com>
2022-08-03 18:33:17 +0200

    edit schema

--- server/models/gallery.model.js
+++ server/models/gallery.model.js
@@ -5,8 +5,11 @@ const gallerySchema = new mongoose.Schema(
     title: { type: String },
     filename: { type: String, required: true, unique: true },
     location: { type: String },
-    source: { name: String, url: String },
-    photographer: { name: String, url: String },
+    photographer: { name: { type: String, required: true }, url: String },
+    votes: { type: Number, required: true, default: 0 }, //Numero di voti effettuati
+    averageVote: { type: Number, required: true, default: 0 }, //Media dei voti
+    votesNumber: { type: Number, required: true, default: 0 }, //numero dei votanti
+    username: { type: String, required: true },
   },
   {
     collection: "gallery-data",
