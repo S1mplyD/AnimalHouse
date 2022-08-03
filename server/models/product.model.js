d4c178dd8ed377cbf3a30ba9f53c6313fc725804
Luca Bennati <lucabennati2k16@gmail.com>
2022-08-03 18:32:55 +0200

    refactor

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -2,8 +2,8 @@ const { default: mongoose } = require("mongoose");
 
 const productSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    description: { type: String, required: true },
+    title: { type: String, required: true },
+    info: { type: String, required: true },
     price: { type: Number, required: true },
     discountedPrice: { type: Number },
     categories: { type: [String] },
