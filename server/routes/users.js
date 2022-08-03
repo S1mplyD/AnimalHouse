a52f13c9c3918d79560bf336fa83e83f114c484d
Luca Bennati <lucabennati2k16@gmail.com>
2022-08-03 18:28:50 +0200

    add logs, fix queries, add premissions function

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const User = require("../models/user.model");
-const bcrypt = require("bcryptjs");
 
 router
   .route("/")
@@ -9,34 +8,13 @@ router
    * Ottieni tutti gli utenti
    */
   .get(async (req, res) => {
-    const users = await User.find();
-    res.send(users);
-  })
-  /**
-   * POST
-   * Crea un nuovo utente
-   */
-  .post(async (req, res) => {
-    const encryptedPassword = await bcrypt.hash(req.body.password, 10);
     try {
-      await User.create({
-        name: req.body.name,
-        surname: req.body.surname,
-        username: req.body.username,
-        mail: req.body.mail,
-        password: encryptedPassword, //Password criptata usando hash SHA512. Utilizzare bcrypt.compare(password,encryptedPassword) per ottenere il plaintext
-        ownedAnimals: req.body.ownedAnimals, //Id degli animali posseduti (Utilizza il campo _id di mongo)
-        profilePicture: req.body.profilePicture, //Array di path al filesystem dove sono contenute le immagini
-        admin: false, //Il privilegio di admin deve essere dato da un superiore
-      });
-      res.sendStatus(200);
+      const users = await User.find();
+      res.send(users);
     } catch (error) {
-      res.json({ Error: "Error" + error });
+      console.log(error);
     }
-  });
-
-router
-  .route("/:userid")
+  })
   /**
    * PATCH
    * Cambia una o più proprietà di un utente
@@ -45,10 +23,10 @@ router
    */
   .patch(async (req, res) => {
     try {
-      await User.findByIdAndUpdate(req.params.userid, req.body);
+      await User.findByIdAndUpdate(req.query.id, req.body);
       res.status(200).json("Utente modificato con successo");
     } catch (error) {
-      res.json({ Error: "Errore: " + error });
+      console.log(error);
     }
   })
   /**
@@ -59,34 +37,71 @@ router
    */
   .delete(async (req, res) => {
     try {
-      await User.findByIdAndDelete(req.params.userid);
+      await User.findByIdAndDelete(req.query.id);
       res.status(200).json("Utente cancellato con successo");
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+router
+  .route("/user")
+  /**
+   * GET
+   * Ottieni un utente dato un parametro
+   */
+  .get(async (req, res) => {
+    try {
+      const user = await User.findOne({
+        $or: [
+          { username: req.query.userfield },
+          { name: req.query.userfield },
+          { surname: req.query.userfield },
+          { mail: req.query.userfield },
+        ],
+      }).then(() => {
+        res.status(200).json(user);
+      });
     } catch (error) {
       res.json({ Error: "Errore: " + error });
     }
   });
 
 router
-  .route("/:userfield")
+  .route("/grantPermission")
   /**
-   * GET
-   * Ottieni un utente dato un parametro
-   *
-   * @param userfield parametro dell'utente
+   * POST
+   * grant admin permission to user
    */
-  .get(async (req, res) => {
-    try {
-      const user = await User.findOne({
-        $or: [
-          { username: req.params.userfield },
-          { name: req.params.userfield },
-          { surname: req.params.userfield },
-          { mail: req.params.userfield },
-        ],
+  .patch(async (req, res) => {
+    if (req.user.admin) {
+      await User.findOneAndUpdate(
+        { username: req.body.username },
+        { admin: true }
+      ).then(() => {
+        res.status(200);
       });
-      res.status(200).json(user);
-    } catch (error) {
-      res.json({ Error: "Errore: " + error });
+    } else {
+      res.status(401).send("Permission denied");
+    }
+  });
+
+router
+  .route("/revokePermission")
+  /**
+   * POST
+   * revoke admin permission to user
+   */
+  .patch(async (req, res) => {
+    if (req.user.admin) {
+      await User.findOneAndUpdate(
+        { username: req.body.username },
+        { admin: false }
+      ).then(() => {
+        res.status(200);
+      });
+    } else {
+      res.status(401).send("Permission denied");
     }
   });
 
