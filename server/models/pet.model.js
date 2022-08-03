0e9d152dba64e59aeca62e294d44396e6837cbf0
Luca Bennati <lucabennati2k16@gmail.com>
2022-08-03 22:29:19 +0200

    add default value

--- server/models/pet.model.js
+++ server/models/pet.model.js
@@ -7,7 +7,7 @@ const petSchema = new mongoose.Schema(
     race: { type: String },
     owner: { type: String, required: true }, //username dell'owner
     age: { type: Number },
-    premium: { type: Boolean, required: true },
+    premium: { type: Boolean, required: true, default: false },
     pictures: { type: [String] }, //Path alle immagini su filesystem
   },
   {
