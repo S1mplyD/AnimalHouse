3937e6268262eeba236b2a88769ce2120a19dd12
Luca Bennati <lucabennati2k16@gmail.com>
2022-08-07 17:17:56 +0200

    add services

---
+++ server/routes/services.js
@@ -0,0 +1,119 @@
+const router = require("express").Router();
+const Service = require("../models/services.model");
+
+router
+  .route("/")
+  /**
+   * GET
+   * get all services info
+   */
+  .get(async (req, res) => {
+    try {
+      await Service.find().then((services) => {
+        res.status(200).send(services);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  })
+  /**
+   * POST
+   * create a new service
+   */
+  .post(async (req, res) => {
+    try {
+      if (req.user.admin) {
+        await Service.create({
+          name: req.body.name,
+          location: req.body.location,
+          coordinate: req.body.coordinate,
+          openDays: req.body.openDays,
+          openTime: new Date().setHours(req.body.openTime + 2),
+          closeTime: new Date().setHours(req.body.closeTime + 2),
+          type: req.body.type,
+          info: req.body.info,
+        }).then(() => {
+          res.sendStatus(201);
+        });
+      } else {
+        res.sendStatus(401);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  })
+  /**
+   * PATCH
+   * update a service
+   */
+  .patch(async (req, res) => {
+    try {
+      if (req.user.admin) {
+        await Service.findByIdAndUpdate(req.query.id, {
+          name: req.body.name,
+          location: req.body.location,
+          coordinate: req.body.coordinate,
+          openDays: req.body.openDays,
+          openTime: new Date().setHours(req.body.openTime + 2),
+          closeTime: new Date().setHours(req.body.closeTime + 2),
+          type: req.body.type,
+          info: req.body.info,
+        }).then(() => {
+          res.sendStatus(200);
+        });
+      } else {
+        res.sendStatus(401);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  })
+  /**
+   * DELETE
+   * delete a service
+   */
+  .delete(async (req, res) => {
+    try {
+      if (req.user.admin) {
+        await Service.deleteOne({ _id: req.query.id }).then(() => {
+          res.sendStatus(200);
+        });
+      } else {
+        res.sendStatus(401);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+router
+  .route("/service")
+  /**
+   * GET
+   * get a specific service
+   */
+  .get(async (req, res) => {
+    try {
+      if (req.query.location != "" && req.query.type == "") {
+        await Service.find({ location: req.query.location }).then(
+          (services) => {
+            res.status(200).send(services);
+          }
+        );
+      } else if (req.query.location == "" && req.query.type != "") {
+        await Service.find({ type: req.query.type }).then((services) => {
+          res.status(200).send(services);
+        });
+      } else if (req.query.location != "" && req.query.type != "") {
+        await Service.find({
+          $and: [{ type: req.query.type }, { location: req.query.location }],
+        }).then((services) => {
+          res.status(200).send(services);
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+module.exports = router;
