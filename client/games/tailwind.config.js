6638bed71bc9a3aebcf65962ce6f070ac6c10fce
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2022-08-12 16:53:38 +0200

    update tailwind config

--- client/games/tailwind.config.js
+++ client/games/tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    maxWidth: {
+      "1/4": "25%",
+    },
+    maxWidth: {
+      "08": "80%",
+    },
+    maxHeight: {
+      "08": "80%",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/forms")],
