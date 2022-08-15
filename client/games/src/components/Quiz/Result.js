6443cf003aa7a0b6e6fc3b445244c7e06904b8bd
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2022-08-15 10:44:04 +0200

    remove form

--- client/games/src/components/Quiz/Result.js
+++ client/games/src/components/Quiz/Result.js
@@ -17,26 +17,24 @@ function Result({ score, setScore, game }) {
     <div className="flex flex-col items-center bg-white rounded w-fit m-auto mt-10">
       <div className="m-10 w-fit h-80 flex flex-col items-center content-around border-gray-400 border-solid border-4 p-5 rounded-xl">
         <div className="text-center">Your score is: {score}</div>
-        <div className="text-center my-5">
+        <div className="text-center my-5 flex flex-col items-center">
           <h1>Save your score</h1>
-          <form className="mt-5 rounded-md">
-            <input
-              type="text"
-              name="username"
-              placeholder="username"
-              id="username"
-              className="mt-5 rounded-md"
-            />
-            <input
-              type="submit"
-              className="mt-5 rounded-lg bg-cyan-400 p-2 hover:bg-blue-500"
-              value="submit"
-              onClick={() => {
-                submitScore();
-                navigate("/games");
-              }}
-            />
-          </form>
+          <input
+            type="text"
+            name="username"
+            placeholder="username"
+            id="username"
+            className="mt-5 rounded-md"
+          />
+          <input
+            type="submit"
+            className="mt-5 rounded-lg bg-cyan-400 p-2 hover:bg-blue-500"
+            value="submit"
+            onClick={() => {
+              submitScore();
+              navigate("/games");
+            }}
+          />
         </div>
       </div>
     </div>
