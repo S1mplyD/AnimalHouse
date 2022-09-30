cb7eb57ee52fc1525df2c831adc386c7e5149810
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2022-09-30 14:37:20 +0200

    update ads component

--- client/games/src/components/ADs.js
+++ client/games/src/components/ADs.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-export default function ADs(products) {
-  console.log(products.products);
+export default function ADs(ads) {
+  console.log(ads.ads);
+  const ad = ads.ads[0];
+  let randomAd = ads.ads[Math.floor(Math.random() * ads.ads.length)];
+  console.log(randomAd.photos);
+
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-white">{products.products[0].info}</h1>
+      {/* <img
+        src={require("/home/luca/Documents/GitHub/AnimalHouse/server/Images/" +
+          randomAd.photos[0])}
+        alt={randomAd.title}
+      ></img> */}
+      <h1 className="text-white">{randomAd.info}</h1>
     </div>
   );
 }
