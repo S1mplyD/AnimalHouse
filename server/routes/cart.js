14693c6bcacb9583be0a40f012b153abb4a11d45
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2022-11-21 17:01:09 +0100

    add patch, delete and update post
    added check to see if the product already exist in the cart

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -33,28 +33,105 @@ router
    *
    * Add a product to the cart
    */
-  //TODO: check if product is already added to the cart
   .post(async (req, res) => {
     try {
       if (req.user != null) {
         await Product.findById(req.query.productid).then(async (product) => {
-          console.log(product);
-          await Cart.create({
-            title: product.title,
-            price: product.price,
-            discountedPrice: product.discountedPrice,
-            mainPhoto: product.mainPhoto,
-            quantity: req.query.quantity,
-          }).then(async (cart) => {
-            await User.findOneAndUpdate(
-              { username: req.user.username },
-              { $push: { cartProducts: cart._id } }
-            );
-            res.sendStatus(201);
+          await Cart.findOne({ productId: product._id }).then(async (prod) => {
+            if (prod) {
+              await Cart.findOneAndUpdate(
+                {
+                  $and: [
+                    {
+                      _id: { $in: req.user.cartProducts },
+                    },
+                    { productId: prod.productId },
+                  ],
+                },
+                {
+                  $inc: { quantity: 1 },
+                }
+              ).then(() => {
+                res.sendStatus(200);
+              });
+            } else {
+              await Cart.create({
+                title: product.title,
+                price: product.price,
+                discountedPrice: product.discountedPrice,
+                mainPhoto: product.mainPhoto,
+                productId: product._id,
+                quantity: req.query.quantity,
+              }).then(async (cart) => {
+                await User.findOneAndUpdate(
+                  { username: req.user.username },
+                  { $push: { cartProducts: cart._id } }
+                );
+                res.sendStatus(201);
+              });
+            }
           });
         });
       } else {
         res.sendStatus(401);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  })
+  //Need more tests with different users
+  /**
+   * PATCH
+   *
+   * edit the quantity of a product in the cart
+   */
+  .patch(async (req, res) => {
+    try {
+      if (req.user != null) {
+        await Cart.findOneAndUpdate(
+          {
+            $and: [
+              { _id: req.query.id },
+              { _id: { $in: req.user.cartProducts } },
+            ],
+          },
+          {
+            quantity: req.query.quantity,
+          }
+        ).then((result) => {
+          if (result) {
+            res.sendStatus(200);
+          } else {
+            res.sendStatus(401);
+          }
+        });
+      } else {
+        res.sendStatus(401);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  })
+  /**
+   * DELETE
+   *
+   * remove a product from the cart
+   */
+  .delete(async (req, res) => {
+    try {
+      if (req.user != null) {
+        await Cart.findOneAndRemove({
+          $and: [
+            { _id: req.query.id },
+            { _id: { $in: req.user.cartProducts } },
+          ],
+        }).then((result) => {
+          if (result) {
+            res.sendStatus(200);
+          } else {
+            res.sendStatus(401);
+          }
+        });
       }
     } catch (error) {
       console.log(error);
