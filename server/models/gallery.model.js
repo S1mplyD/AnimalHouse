c632ceccdaca4a0778ec0cd7c4acd5b9e5573bbb
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2022-11-23 17:52:09 +0100

    add comments

--- server/models/gallery.model.js
+++ server/models/gallery.model.js
@@ -3,13 +3,18 @@ const mongoose = require("mongoose");
 const gallerySchema = new mongoose.Schema(
   {
     title: { type: String },
-    filename: { type: String, required: true, unique: true },
-    location: { type: String },
-    photographer: { name: { type: String, required: true }, url: String },
+    filename: {
+      type: String,
+      required: true,
+      unique: true,
+      default: "placeholder",
+    },
+    location: { type: String }, //name of the place (?)
+    photographer: { name: { type: String, required: true }, url: String }, //photographer name and url to social media/site
     votes: { type: Number, required: true, default: 0 }, //Numero di voti effettuati
     averageVote: { type: Number, required: true, default: 0 }, //Media dei voti
     votesNumber: { type: Number, required: true, default: 0 }, //numero dei votanti
-    username: { type: String, required: true },
+    username: { type: String, required: true }, //user that added the image to the gallery
   },
   {
     collection: "gallery-data",
