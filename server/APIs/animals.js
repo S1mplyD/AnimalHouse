99eb74db00c004ab05e7aa4b7555b7351b46a35c
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2022-12-22 11:58:07 +0100

    add animals functions

--- server/APIs/animals.js
+++ server/APIs/animals.js
@@ -1,14 +1,82 @@
+const Animal = require("../models/animals.model");
 const botaniczoo = require("botanic-zoo-api");
-
 async function getAllAnimals() {
-  let cats = await botaniczoo.getCatPets();
-  let dogs = await botaniczoo.getDogPets();
-  let birds = await botaniczoo.getBirdPets();
-  let exotic = await botaniczoo.getExoticPets();
-  let rodents = await botaniczoo.getExoticPets();
-  let fish = await botaniczoo.getFishPets();
-  let allAnimals = [cats, dogs, birds, exotic, rodents, fish];
-  return allAnimals;
+  let animals = await Animal.find();
+  console.log(animals.length);
 }
 
-module.exports = { getAllAnimals };
+async function getMemoryAnimals() {
+  let animals = await Animal.find();
+  let memoryAnimals = [];
+  for (let i = 0; i < 8; i++) {
+    memoryAnimals.push(
+      animals[Math.floor(Math.random() * animals.length)].image
+    );
+  }
+  console.log(memoryAnimals);
+}
+
+// async function populateDb() {
+//   await botaniczoo
+//     .getBirdPets()
+//     .then((birds) => {
+//       for (let i of birds.Pets) {
+//         Animal.create({
+//           name: i.Name,
+//           info: "Bird",
+//           funFact: i.FunFact,
+//           image: i.ImageUrl,
+//         });
+//       }
+//     })
+//     .then(async () => {
+//       await botaniczoo.getCatPets().then((cats) => {
+//         for (let i of cats.Pets) {
+//           Animal.create({
+//             name: i.Name,
+//             info: "Cats",
+//             funFact: i.FunFact,
+//             image: i.ImageUrl,
+//           });
+//         }
+//       });
+//     })
+//     .then(async () => {
+//       await botaniczoo.getDogPets().then((dogs) => {
+//         for (let i of dogs.Pets) {
+//           Animal.create({
+//             name: i.Name,
+//             info: "Dogs",
+//             funFact: i.FunFact,
+//             image: i.ImageUrl,
+//           });
+//         }
+//       });
+//     })
+//     .then(async () => {
+//       await botaniczoo.getFishPets().then((fish) => {
+//         for (let i of fish.Pets) {
+//           Animal.create({
+//             name: i.Name,
+//             info: "Fish",
+//             funFact: i.FunFact,
+//             image: i.ImageUrl,
+//           });
+//         }
+//       });
+//     })
+//     .then(async () => {
+//       await botaniczoo.getRodentPets().then((rodents) => {
+//         for (let i of rodents.Pets) {
+//           Animal.create({
+//             name: i.Name,
+//             info: "Rodents",
+//             funFact: i.FunFact,
+//             image: i.ImageUrl,
+//           });
+//         }
+//       });
+//     });
+// }
+
+module.exports = { getAllAnimals, getMemoryAnimals };
