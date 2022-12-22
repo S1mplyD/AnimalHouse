3348caefaa4143e42381b37bd5cfdb176ed07ba7
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2022-12-22 11:57:48 +0100

    remove unused dependencies

--- games/src/components/Home.js
+++ games/src/components/Home.js
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-// import path from "path";
 
 export default function Home({
   getQuestions,
