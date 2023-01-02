c53111ee1a4ff072e2b0510492eb7d744edc8f37
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-01-02 16:41:24 +0100

    add white bg to ads

--- games/src/components/ADs.js
+++ games/src/components/ADs.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Ads(title, image, info) {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
       <img src={`/${title.image}`} alt={title.title} className="w-full" />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{title.title}</div>
