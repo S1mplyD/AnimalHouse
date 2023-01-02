09b3ba08a55e3006c10046ac38353b370bde3e4f
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-01-02 15:28:25 +0100

    fix ad's image

--- games/src/components/ADs.js
+++ games/src/components/ADs.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Ads(title, image, info) {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src={title.image} alt={title.title} className="w-80 h-80" />
+      <img src={`/${title.image}`} alt={title.title} className="w-full" />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{title.title}</div>
         <p className="text-gray-700 text-base">{title.info}</p>
