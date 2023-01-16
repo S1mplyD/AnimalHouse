1f7769655ed44ce7b54c447a047bace5c0acd0ae
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-01-16 11:40:50 +0100

    clickable ad

--- games/src/components/ADs.js
+++ games/src/components/ADs.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-export default function Ads(title, image, info) {
+export default function Ads(ad) {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-      <img src={`/${title.image}`} alt={title.title} className="w-full" />
+        {console.log(ad.ad)}
+        <a href={`/shop/${ad.ad._id}`}>
+      <img src={`/${ad.ad.image}`} alt={ad.ad.title} className="w-full" />
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{title.title}</div>
-        <p className="text-gray-700 text-base">{title.info}</p>
+        <div className="font-bold text-xl mb-2">{ad.ad.title}</div>
+        <p className="text-gray-700 text-base">{ad.ad.info}</p>
       </div>
+        </a>
     </div>
   );
 }
