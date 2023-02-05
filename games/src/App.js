15931259e4f762596e76cd56dcb639af1e79bada
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-02-05 16:36:53 +0100

    update routes and data fetching

--- games/src/App.js
+++ games/src/App.js
@@ -1,189 +1,56 @@
-import {Routes, Route, Link} from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import React, {useState} from "react";
-import axios from "axios";
-import {useEffect} from "react";
 import Quiz from "./components/Quiz/Quiz";
 import Home from "./components/Home";
 import Result from "./components/Quiz/Result";
 import Memory from "./components/Memory/Memory";
 import Hangman from "./components/Impiccato/hangman";
-import Test from "./components/test"
 
 function App() {
-    const [questions, setQuestions] = useState([]);
-    const [score, setScore] = useState(0);
-    const [images, setImages] = useState([]);
-    const [word, setWord] = useState();
-    const [game, setGame] = useState("");
-    const [ads, setAds] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [currentAds, setCurrentAds] = useState(0)
-
-    useEffect(() => {
-        console.log(sessionStorage)
-
-        async function ads() {
-            await getAds(
-                sessionStorage.getItem("specie"),
-                sessionStorage.getItem("name"),
-                sessionStorage.getItem("gender"),
-                sessionStorage.getItem("age"),
-                sessionStorage.getItem("medicalCondition"))
-        }
-
-        ads();
-    }, []);
-
-    const getAds = async (specie, name, gender, age, medicalCondition) => {
-        await axios.get("/api/ads", {
-            params: {
-                specie: specie, name: name, gender: gender, age: age, medicalCondition: medicalCondition
-            }
-        }).then((res) => {
-            if (res.data != null) {
-                for (let i = res.data.length - 1; i > 0; i--) {
-                    const j = Math.floor(Math.random() * (i + 1));
-                    [res.data[i], res.data[j]] = [res.data[j], res.data[i]];
-                }
-                setAds(res.data);
-                setLoading(false);
-            }
-        });
-    };
-
-    const getQuestions = async () => {
-        const rawData = await axios.get("/api/getTrivia");
-        setQuestions(rawData.data);
-    };
-
-    const getImages = async () => {
-        const data = await axios.get("/api/animals/memory");
-        let memoryImages = [];
-        data.data.map((i, index) => {
-            memoryImages.push({
-                id: index, image: i.image, state: "", name: i.name,
-            });
-            memoryImages.push({
-                id: index, image: i.image, state: "", name: i.name,
-            });
-        });
-        memoryImages.sort(() => Math.random() - 0.5);
-        console.log(memoryImages);
-        setImages(memoryImages);
-    };
-
-    const getWords = async () => {
-        await axios.get("/api/animals/hangman").then((data) => {
-            console.log(data.data);
-
-            setWord(data.data.replace(" ", ""));
-        });
-    };
-
+    const [game, setGame] = useState("")
+    const [score, setScore] = useState(0)
     return (
-        <div className="bg-blue-500 overflow-auto h-screen">
-            <div className="flex flex-row justify-evenly">
-                <Link
-                    to="/games"
-                    onClick={() => {
-                        setGame("");
-                        if (currentAds < ads.length - 1) {
-                            setCurrentAds(currentAds + 1)
-                        } else {
-                            setCurrentAds(0)
-                        }
-                        setScore(0);
-                    }}
-                >
-                    <h1 className="text-white flex flex-col items-center text-3xl">
-                        Animal House Games
-                    </h1>
-                </Link>
-                <a href="/shop">
-                    <h1 className="text-white flex flex-col items-center text-3xl">
-                        Visit our shop!
-                    </h1>
-                </a>
-                <a href={"/games/test"}>Test</a>
+        <BrowserRouter>
+            <div className="bg-blue-500 overflow-auto h-screen">
+                <div className="flex flex-row justify-evenly">
+                    <Link
+                        to="/games"
+                    >
+                        <h1 className="text-white flex flex-col items-center text-3xl">
+                            Animal House Games
+                        </h1>
+                    </Link>
+                    <a href="/shop">
+                        <h1 className="text-white flex flex-col items-center text-3xl">
+                            Visit our shop!
+                        </h1>
+                    </a>
+                    <Link to={"/games/test"}><h1>test</h1></Link>
+                </div>
+                <Routes>
+                    <Route
+                        path="/games"
+                        element={<Home/>}
+                    />
+                    <Route
+                        path="games/quiz"
+                        element={<Quiz setScore={setScore} setGame={setGame} score={score}/>}
+                    />
+                    <Route
+                        path="games/result"
+                        element={<Result setGame={setGame} game={game} score={score} setScore={setScore}/>}
+                    />
+                    <Route
+                        path="games/memory"
+                        element={<Memory setScore={setScore} setGame={setGame} score={score}/>}
+                    ></Route>
+                    <Route
+                        path="games/hangman"
+                        element={<Hangman setScore={setScore} setGame={setGame} score={score}/>}
+                    ></Route>
+                </Routes>
             </div>
-
-            <Routes>
-                <Route
-                    path="/games"
-                    element={<Home
-                        setQuestions={setQuestions}
-                        getQuestions={getQuestions}
-                        setScore={setScore}
-                        getImages={getImages}
-                        getWords={getWords}
-                        setGame={setGame}
-                        loading={loading}
-                        ads={ads}
-                        currentAd={currentAds}
-                        setCurrentAds={setCurrentAds}
-                    />}
-                />
-                <Route
-                    path="/games/quiz"
-                    element={<Quiz
-                        questions={questions}
-                        setQuestions={setQuestions}
-                        score={score}
-                        setScore={setScore}
-                        setGame={setGame}
-                        ads={ads}
-                        setCurrentAds={setCurrentAds}
-                        currentAd={currentAds}
-                    />}
-                />
-                <Route
-                    path="/result"
-                    element={<Result
-                        score={score}
-                        setScore={setScore}
-                        setGame={setGame}
-                        game={game}
-                        ads={ads}
-                        currentAd={currentAds}
-                        setCurrentAds={setCurrentAds}
-                    />}
-                />
-                <Route
-                    path="/games/memory"
-                    element={<Memory
-                        images={images}
-                        setImages={setImages}
-                        score={score}
-                        setScore={setScore}
-                        ads={ads}
-                        currentAd={currentAds}
-                        setCurrentAds={setCurrentAds}
-                    />}
-                ></Route>
-                <Route
-                    path="/games/hangman"
-                    game={game}
-                    element={<Hangman
-                        word={word}
-                        setWord={setWord}
-                        setScore={setScore}
-                        score={score}
-                        ads={ads}
-                        currentAd={currentAds}
-                        setCurrentAds={setCurrentAds}
-                        setAds={setAds}
-                    ></Hangman>}
-                ></Route>
-                <Route
-                    path={"/games/test"}
-                    element={
-                        <Test/>
-                    }
-                >
-                </Route>
-            </Routes>
-
-        </div>
+        </BrowserRouter>
     );
 }
 
