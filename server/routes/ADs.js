63f6ede2d5e1afbeaf1cb1b15ac7a939dcb6f602
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-02-05 16:36:01 +0100

    update ads api

--- server/routes/ADs.js
+++ server/routes/ADs.js
@@ -5,8 +5,8 @@ router
     .route("/")
     //Get custom or random ad
     .get(async (req, res) => {
-        console.log(req.query)
         let arr = [];
+        console.log(req.query)
         for (let i in req.query) {
             if (req.query[i] != "") {
                 arr.push(req.query[i]);
@@ -20,26 +20,36 @@ router
         } else {
             Products.find().then((products) => {
                 let ads = [];
+                // index of products
                 for (let i in products) {
+                    // fields of products
                     for (let k in products[i].toJSON()) {
-                        if (typeof products[i].toJSON()[k] === "string") {
+                        // value of field
+                        if (typeof products[i].toJSON()[k] === "string" && (k === "info" || k === "title")) {
                             arr.forEach((el) => {
-                                if (products[i].toJSON()[k].includes(el)) {
+                                let string = products[i].toJSON()[k].toLowerCase()
+                                if (string.includes(el.toLowerCase()) ) {
                                     ads.push(products[i].toJSON());
                                 }
                             });
                         }
+
                     }
                     //Controllo le categorie
-                    arr.forEach((el) => {
-                        if (products[i].toJSON().categories.includes(el)) {
-                            ads.push(products[i].toJSON());
-                        }
-                    });
+                    // index of category
+                    for (let j in products[i].toJSON().categories){
+                        arr.forEach((el) => {
+                            if (products[i].toJSON().categories[j].toLowerCase() === el.toLowerCase()) {
+                                ads.push(products[i].toJSON());
+                            }
+                        });
+                    }
+
                 }
-                let response = []
                 //Remove duplicates
+                // index of ads
                 for(let i in ads){
+                    // ads
                     for(let j = Number(i) + 1; j < ads.length; j++){
                         if(ads[i]._id === ads[j]._id){
                             ads.splice(Number(i),1)
@@ -47,8 +57,8 @@ router
                     }
                 }
                 //If there are items similar to the described pet
-                if (!response.length < 1) {
-                    res.send(response);
+                if (ads.length > 0) {
+                    res.send(ads);
                 } else {
                     // if nothing is found
                     res.send(products)
