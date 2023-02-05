1bacab5c9e91a4ed4ee6b9862bb82b7d84c0b904
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-02-05 16:37:44 +0100

    add new data fetch logic

--- games/src/components/Impiccato/hangman.js
+++ games/src/components/Impiccato/hangman.js
@@ -1,144 +1,155 @@
 import React, {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import Ads from "../ADs";
+import {getAds, getServices, getWords} from "../../apiCalls";
+import Services from "../Services";
 
-export default function Hangman({word, setWord, setScore, score, setGame, currentAd, setCurrentAds, ads}) {
-    const [correctWord, setCorrectWord] = useState(word.split(""));
-    const [wordarr, setWordArr] = useState(word.split(""));
+export default function Hangman({setGame, score, setScore}) {
+    const [correctWord, setCorrectWord] = useState("");
+    const [wordarr, setWordArr] = useState("");
+    const [word, setWord] = useState("")
+    const [loading, setLoading] = useState(true)
+    const [first, setFirst] = useState(true)
+    const [ads, setAds] = useState([])
+    const [services, setServices] = useState([])
+    const [hangmanState, setHangmanState] = useState(0);
+
     const navigate = useNavigate();
-    const handleQuit = () => {
-        setWord("");
-        setScore(0);
-        if (currentAd < ads.length - 1) {
-            setCurrentAds(currentAd + 1)
-        } else {
-            setCurrentAds(0)
+
+    useEffect(() => {
+        async function fetchData() {
+            let rawServices = await getServices()
+            setServices(rawServices.data)
+            const rawAds = await getAds(
+                sessionStorage.getItem("specie"),
+                sessionStorage.getItem("name"),
+                sessionStorage.getItem("gender"),
+                sessionStorage.getItem("age"),
+                sessionStorage.getItem("medicalCondition")
+            )
+            setAds(rawAds.data)
+            let words = await getWords()
+            let arr = words.split("")
+            console.log(arr)
+            setWord(words)
+            setCorrectWord(words.split(""))
+            arr.forEach((el, i) => {
+                arr[i] = "_"
+            })
+            setWordArr(arr)
         }
+
+        fetchData().then(() => {
+            setLoading(false)
+        })
+
+    }, [])
+
+    const handleQuit = () => {
+        setScore(0);
         navigate("/games");
     };
-    const [hangmanState, setHangmanState] = useState(0);
-    useEffect(() => {
-        console.log(wordarr);
-        let arr = [];
-        wordarr.forEach((el, i) => {
-            arr[i] = "_";
-        });
-        setWordArr(arr);
-        document.getElementById("hiddenWord").innerText = arr;
-    }, []);
 
     const handleLose = () => {
+        alert("you lose")
         setScore(0);
+        setGame("")
         console.log("handle lose");
-        alert("you lose");
-        if (currentAd < ads.length) {
-            setCurrentAds(currentAd + 1)
-        } else {
-            setCurrentAds(0)
-        }
-        navigate("/result");
+        navigate("/games");
     };
-    //TODO sistemare punteggi
+
     const checkInput = () => {
         let inputWord = document.getElementById("inputWord");
-        if (hangmanState >= 6) {
-            console.log("check input");
-            alert("You lose");
-            setScore(0);
-            if (currentAd < ads.length) {
-                setCurrentAds(currentAd + 1)
+        if (inputWord.value.length > 1) {
+            if (
+                word.localeCompare(inputWord.value, "en", {sensitivity: "base"}) ===
+                0
+            ) {
+                alert("You found the correct word");
+                setScore(word.length);
+                setGame("hangman");
+                navigate("/games/result");
             } else {
-                setCurrentAds(0)
+                alert("wrong word");
+                document.getElementById("inputWord").value = "";
+                setHangmanState(hangmanState + 1);
             }
-            navigate("/result");
         } else {
-            if (inputWord.value.length > 1) {
-                if (
-                    word.localeCompare(inputWord.value, "en", {sensitivity: "base"}) ===
-                    0
-                ) {
-                    alert("You found the correct word");
-                    setScore(word.length);
-                    setGame("hangman");
-                    if (currentAd < ads.length) {
-                        setCurrentAds(currentAd + 1)
-                    } else {
-                        setCurrentAds(0)
-                    }
-                    navigate("/result");
-                } else {
-                    alert("wrong word");
-                    document.getElementById("inputWord").value = "";
-                    setHangmanState(hangmanState + 1);
+            if (word.toLowerCase().includes(inputWord.value.toLowerCase())) {
+                showLetter(inputWord.value);
+                document.getElementById("inputWord").value = "";
+                setScore(score + 1);
+                if (!wordarr.includes("_")) {
+                    setGame("hangman")
+                    navigate("/games/result");
                 }
             } else {
-                console.log("input: " + inputWord.value);
-                if (word.toLowerCase().includes(inputWord.value.toLowerCase())) {
-                    showLetter(inputWord.value);
-                    document.getElementById("inputWord").value = "";
-                    setScore(score + 1);
-                    if (!wordarr.includes("_")) {
-                        if (currentAd < ads.length) {
-                            setCurrentAds(currentAd + 1)
-                        } else {
-                            setCurrentAds(0)
-                        }
-                        navigate("/result");
-                    }
-                } else {
-                    document.getElementById("inputWord").value = "";
-                    setHangmanState(hangmanState + 1);
-                }
+                document.getElementById("inputWord").value = "";
+                setHangmanState(hangmanState + 1);
             }
+
         }
     };
 
     const showLetter = (letter) => {
         let correct = 0;
+        console.log(correctWord)
         correctWord.forEach((el, index) => {
             if (el.localeCompare(letter, "en", {sensitivity: "base"}) === 0) {
                 wordarr[index] = el;
-                console.log("inside showLetter: " + wordarr);
                 correct++;
             }
-            // setScore(score + correct);
+            setScore(score + correct);
             document.getElementById("hiddenWord").innerText = wordarr;
         });
     };
-    return (
-        <div className="flex flex-col items-center bg-white rounded m-2 my-20 md:mx-52">
-            <img src={require(`./Images/${hangmanState}.png`)} alt=""></img>
 
-            <p className=" text-center fs-2" id="hiddenWord">
-                {wordarr}
-            </p>
-            {hangmanState >= 6 ? handleLose() : null}
-            <input
-                type="text"
-                name="inputWord"
-                id="inputWord"
-                placeholder="Insert a character or an entire word here"
-                className=" my-10"
-            />
-            <div>
-                <button
-                    type="button"
-                    className="p-3 m-3 border-solid border-4 rounded-xl border-black"
-                    onClick={checkInput}
-                >
-                    Check letter
-                </button>
-                <button
-                    type="button"
-                    className="p-3 m-3 border-solid border-4 rounded-xl border-black"
-                    onClick={handleQuit}
-                >
-                    Quit
-                </button>
-            </div>
-            <Ads
-                ad={ads[currentAd]}
-            ></Ads>
-        </div>
-    );
+    if (!loading) {
+        return (
+            <>
+                <div className="flex flex-col items-center bg-white rounded m-2 my-20 md:mx-52">
+                    <img src={require(`./Images/${hangmanState}.png`)} id={"hangmanImg"} alt={`error ${hangmanState}`}></img>
+
+                    <p className=" text-center fs-2" id="hiddenWord">
+                        {wordarr}
+                    </p>
+
+                    {hangmanState >= 6 ? handleLose() : null}
+                    <input
+                        type="text"
+                        name="inputWord"
+                        id="inputWord"
+                        placeholder="Insert a character or an entire word here"
+                        className=" my-10"
+                    />
+                    <div>
+                        <button
+                            type="button"
+                            className="p-3 m-3 border-solid border-4 rounded-xl border-black"
+                            onClick={checkInput}
+                        >
+                            Check letter
+                        </button>
+                        <button
+                            type="button"
+                            className="p-3 m-3 border-solid border-4 rounded-xl border-black"
+                            onClick={handleQuit}
+                        >
+                            Quit
+                        </button>
+                    </div>
+
+
+                </div>
+                <div className={"flex flex-row justify-evenly"}>
+                    <Ads
+                        ad={ads}
+                    ></Ads>
+                    <Services service={services}>
+
+                    </Services>
+                </div>
+            </>
+        );
+    }
 }
