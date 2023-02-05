da5307b2ab29cc645d0c8b55cf9f017c05de2f1e
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-02-05 16:38:01 +0100

    update service component

--- games/src/components/Services.js
+++ games/src/components/Services.js
@@ -1,11 +1,10 @@
 import React from "react";
 
-export default function Services(service){
-    return(
+export default function Services(service) {
+    return (
         <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-            {console.log(service)}
             <a href={`/servizi/${service.service[0]._id}`}>
-                <img src={`/${service.service[0].pictures[0]}`} alt={service.service[0].name} className="w-full" />
+                <img src={`/${service.service[0].pictures[0]}`} alt={service.service[0].name} className="w-full"/>
                 <div className="px-6 py-4">
                     <div className="font-bold text-xl mb-2">{service.service[0].name}</div>
                     <p className="text-gray-700 text-base">{service.service[0].info}</p>
