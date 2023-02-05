59adf17d24cb209be30f0dd559079f63bda6f1f4
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-02-05 16:37:11 +0100

    add ads randomizer

--- games/src/components/ADs.js
+++ games/src/components/ADs.js
@@ -1,16 +1,19 @@
 import React from "react";
 
 export default function Ads(ad) {
-  return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-        {console.log(ad.ad)}
-        <a href={`/shop/${ad.ad._id}`}>
-      <img src={`/${ad.ad.image}`} alt={ad.ad.title} className="w-full" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{ad.ad.title}</div>
-        <p className="text-gray-700 text-base">{ad.ad.info}</p>
-      </div>
-        </a>
-    </div>
-  );
+
+    const random = Math.floor(Math.random() * ad.ad.length)
+    return (
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+            <a href={`/shop/${ad.ad[random]._id}`}>
+                <img src={`/${ad.ad[random].image}`}
+                     alt={ad.ad[random].title} className="w-full"/>
+                <div className="px-6 py-4">
+                    <div
+                        className="font-bold text-xl mb-2">{ad.ad[random].title}</div>
+                    <p className="text-gray-700 text-base">{ad.ad[random].info}</p>
+                </div>
+            </a>
+        </div>
+    );
 }
