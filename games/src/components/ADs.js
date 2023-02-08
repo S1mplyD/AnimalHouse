990eb543d9bd77f51f2d3bb7450ade37003448ad
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-02-08 10:56:25 +0100

    add clickable ad

--- games/src/components/ADs.js
+++ games/src/components/ADs.js
@@ -1,11 +1,12 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Ads(ad) {
 
     const random = Math.floor(Math.random() * ad.ad.length)
     return (
         <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-            <a href={`/shop/${ad.ad[random]._id}`}>
+            <Link to={`/games/shop/${ad.ad[random]._id}`}>
                 <img src={`/${ad.ad[random].image}`}
                      alt={ad.ad[random].title} className="w-full"/>
                 <div className="px-6 py-4">
@@ -13,7 +14,7 @@ export default function Ads(ad) {
                         className="font-bold text-xl mb-2">{ad.ad[random].title}</div>
                     <p className="text-gray-700 text-base">{ad.ad[random].info}</p>
                 </div>
-            </a>
+            </Link>
         </div>
     );
 }
