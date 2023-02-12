39a4394b288d902d99b07c80b398b8dd85e3155e
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-02-12 16:08:51 +0100

    add funfact route

--- server/routes/animals.js
+++ server/routes/animals.js
@@ -1,9 +1,23 @@
 const router = require("express").Router();
 const animals = require("../APIs/animals");
+const { getAnimals } = require("../APIs/animals");
 
 router.route("/").get(async (req, res) => {
-  let animal = await animals.getAllAnimals();
-  res.send(animal);
+  try {
+    let animal = await animals.getAllAnimals();
+    res.send(animal);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.route("/funfact").get(async (req, res) => {
+  try {
+    const animals = await getAnimals(req.query.name, req.query.specie);
+    res.send(animals);
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 router.route("/memory").get(async (req, res) => {
