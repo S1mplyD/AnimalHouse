a7bc918af67d078f6f6b1f969907aed7a23fb09e
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-02-13 17:18:41 +0100

    fix endpoint

--- games/src/apiCalls.js
+++ games/src/apiCalls.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function getAds(specie, name, gender, age, medicalCondition) {
-  let data = await axios.get("http://localhost:8000/api/ads", {
+  let data = await axios.get("/api/ads", {
     params: {
       specie: specie,
       name: name,
@@ -20,7 +20,7 @@ export async function getAds(specie, name, gender, age, medicalCondition) {
 }
 
 export async function getServices() {
-  let data = await axios.get("http://localhost:8000/api/services");
+  let data = await axios.get("/api/services");
   for (let i = data.data.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [data.data[i], data.data[j]] = [data.data[j], data.data[i]];
@@ -29,12 +29,12 @@ export async function getServices() {
 }
 
 export async function getWords() {
-  let data = await axios.get("http://localhost:8000/api/animals/hangman");
+  let data = await axios.get("/api/animals/hangman");
   return data.data.replace(" ", "");
 }
 
 export async function getImages() {
-  const data = await axios.get("http://localhost:8000/api/animals/memory");
+  const data = await axios.get("/api/animals/memory");
   let memoryImages = [];
   data.data.map((i, index) => {
     memoryImages.push({
@@ -55,42 +55,40 @@ export async function getImages() {
 }
 
 export async function getQuestions() {
-  const rawData = await axios.get("http://localhost:8000/api/getTrivia");
+  const rawData = await axios.get("/api/getTrivia");
   return rawData;
 }
 
 export async function getProduct(id) {
-  const rawData = await axios.get(`http://localhost:8000/api/products/${id}`);
+  const rawData = await axios.get(`/api/products/${id}`);
   return rawData;
 }
 
 export async function addToCart(id, quantity) {
-  await axios.post("http://localhost:8000/api/carts", null, {
+  await axios.post("/api/carts", null, {
     params: { productid: id },
   });
 }
 
 export async function login(mail, password) {
-  await axios.post("http://localhost:8000/auth/login", {
+  await axios.post("/auth/login", {
     username: mail,
     password: password,
   });
 }
 
 export async function isAuthenticated() {
-  const user = await axios.get("http://localhost:8000/auth/isAuthenticated");
+  const user = await axios.get("/auth/isAuthenticated");
   return user;
 }
 
 export async function getService(id) {
-  const rawData = await axios.get(
-    `http://localhost:8000/api/services/service/${id}`
-  );
+  const rawData = await axios.get(`/api/services/service/${id}`);
   return rawData;
 }
 
 export async function bookService(id) {
-  await axios.post("http://localhost:8000/api/services/book", null, {
+  await axios.post("/api/services/book", null, {
     params: { id: id },
   });
 }
@@ -99,19 +97,19 @@ export async function getFunFact(name, specie) {
   console.log(name, specie);
   let rawData;
   if (name != null && specie != null) {
-    rawData = await axios.get("http://localhost:8000/api/animals/funfact", {
+    rawData = await axios.get("/api/animals/funfact", {
       params: { name: name, specie: specie },
     });
   } else if (specie != null && name == null) {
-    rawData = await axios.get("http://localhost:8000/api/animals/funfact", {
+    rawData = await axios.get("/api/animals/funfact", {
       params: { name: "", specie: specie },
     });
   } else if (specie == null && name != null) {
-    rawData = await axios.get("http://localhost:8000/api/animals/funfact", {
+    rawData = await axios.get("/api/animals/funfact", {
       params: { name: name, specie: "" },
     });
   } else {
-    rawData = await axios.get("http://localhost:8000/api/animals/funfact", {
+    rawData = await axios.get("/api/animals/funfact", {
       params: { name: "", specie: "" },
     });
   }
