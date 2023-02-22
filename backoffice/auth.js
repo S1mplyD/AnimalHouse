96a523fc944acc1921d3608ec4444178ffbb782e
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-02-22 11:49:44 +0100

    change folder

---
+++ backoffice/auth.js
@@ -0,0 +1,15 @@
+async function login() {
+  const data = {
+    username: document.getElementById("mail").value,
+    password: document.getElementById("pass").value,
+  };
+  fetch("/auth/login", {
+    method: "post",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+}
+
+module.exports = { login };
