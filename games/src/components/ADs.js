8f43fd09cc133a9c23e8e4629c24bdbe1a8c0d8b
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-02-24 11:05:40 +0100

    fix size

--- games/src/components/ADs.js
+++ games/src/components/ADs.js
@@ -1,20 +1,21 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Ads(ad) {
-
-    const random = Math.floor(Math.random() * ad.ad.length)
-    return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-            <Link to={`/games/shop/${ad.ad[random]._id}`}>
-                <img src={`/${ad.ad[random].image}`}
-                     alt={ad.ad[random].title} className="w-full"/>
-                <div className="px-6 py-4">
-                    <div
-                        className="font-bold text-xl mb-2">{ad.ad[random].title}</div>
-                    <p className="text-gray-700 text-base">{ad.ad[random].info}</p>
-                </div>
-            </Link>
+  const random = Math.floor(Math.random() * ad.ad.length);
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+      <Link to={`/games/shop/${ad.ad[random]._id}`}>
+        <img
+          src={`/${ad.ad[random].image}`}
+          alt={ad.ad[random].title}
+          className="w-80 h-80"
+        />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{ad.ad[random].title}</div>
+          <p className="text-gray-700 text-base">{ad.ad[random].info}</p>
         </div>
-    );
+      </Link>
+    </div>
+  );
 }
--- games/src/components/Services.js
+++ games/src/components/Services.js
@@ -1,16 +1,22 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Services(service) {
-    return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-            <Link to={`/games/services/${service.service[0]._id}`}>
-                <img src={`/${service.service[0].pictures[0]}`} alt={service.service[0].name} className="w-full"/>
-                <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">{service.service[0].name}</div>
-                    <p className="text-gray-700 text-base">{service.service[0].info}</p>
-                </div>
-            </Link>
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+      <Link to={`/games/services/${service.service[0]._id}`}>
+        <img
+          src={`/${service.service[0].pictures[0]}`}
+          alt={service.service[0].name}
+          className="w-80 h-80"
+        />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">
+            {service.service[0].name}
+          </div>
+          <p className="text-gray-700 text-base">{service.service[0].info}</p>
         </div>
-    )
+      </Link>
+    </div>
+  );
 }
